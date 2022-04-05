2db5c22351c3edbd6d50e9fe6637c63b301c8902
asoler <asoler@student.42sp.org.br>
2022-04-05 16:58:46 +0200

    new var type char pointer recive void pointer funtion parameter char casting and is returned with void casting

--- ft_memset.c
+++ ft_memset.c
@@ -1,14 +1,18 @@
-void	*ft_memset(void *s, int c, int n)
+#include <stddef.h>
+
+void	*ft_memset(void *s, int c, size_t n)
 {
-	int	i;
+	unsigned int	i;
+	char			*s1;
 
 	i = 0;
+	s1 = (char *) s;
 	while(i < n)
 	{
-		s[i] = c;
+		s1[i] = c;
 		i++;
 	}
-	return (s);
+	return ((void *)s1);
 }
 
 #include <stdio.h>
@@ -19,40 +23,16 @@ int	main()
 	int i = 0;
 
 	printf("%p\n", s);
-	// printf("\n%s\n", ft_memset(s, 'x', 30));
 	while (i < 5)
 	{
 		printf("%d\n", s[i]);
 		i++;
 	}
 	i = 0;
-	// printf("\n%s\n", s);
-	// printf("\n%p\n", s);
-	printf("\n%p\n", ft_memset(s, 'x', 2));
+	printf("\n%p\n", ft_memset(s, 'x', 12));
 	while (i < 5)
 	{
 		printf("%d\n", s[i]);
 		i++;
 	}
-	// printf("\n%p\n", s);
-	
-	// printf("%p\n", s);
-	// while (i < 5)
-	// {
-	// 	printf("%d\n", s[i]);
-	// 	i++;
-	// }
-	// i = 0;
-	// printf("\n%p\n", memset(s, 'x', 2));
-	// while (i < 5)
-	// {
-	// 	printf("%d\n", s[i]);
-	// 	// s++;
-	// 	i++;
-	// }
-
-	// printf("\n%s\n", s);
-	// printf("\n%p\n", s);
-	// printf("\n%p\n", memset(s, 'x', 2));
-	// printf("\n%p\n", s);
 }
