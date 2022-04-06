1dc1b33a1e7a13b7e3966df5c4d13a7c2e6a289e
asoler <asoler@student.42sp.org.br>
2022-04-06 16:49:15 +0200

    ft bzero + better test struct in main

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 15:20:13 by asoler            #+#    #+#             */
-/*   Updated: 2022/04/06 15:28:16 by asoler           ###   ########.fr       */
+/*   Updated: 2022/04/06 16:43:26 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,86 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	
+	unsigned int i;
+	char	*result;
+
+	i = 0;
+	result = (char *)s;
+	while (i < n)
+	{
+		result[i] = 0;
+		i++;
+	}
+	return ((void)result);
+}
+
+#include <stdio.h>
+#include <string.h>
+int	main()
+{
+	int ftx[5] = {49, 49, 49, 49, 49};
+	char fts[5] = {48, 48, 48, 48, 48};
+	char s[5] = {48, 48, 48, 48, 48};
+	int x[5] = {49, 49, 49, 49, 49};
+	int i = 0;
+
+	printf("\n-----------Vetor original int-----------\n\n%p\n", ftx);
+	while (i < 5)
+	{
+		printf("%d\n", ftx[i]);
+		i++;
+	}
+	i = 0;
+	ft_bzero(ftx,8);
+	printf("\n-----------Vetor após função ft int------------\n\n%p\n", ftx);
+	while (i < 5)
+	{
+		printf("%d\n", ftx[i]);
+		i++;
+	}
+	i = 0;
+	printf("\n-----------Vetor original int-----------\n\n%p\n", x);
+	while (i < 5)
+	{
+		printf("%d\n", x[i]);
+		i++;
+	}
+	i = 0;
+	bzero(x, 8);
+	printf("\n-----------Vetor após função original int------------\n\n%p\n", x);
+	while (i < 5)
+	{
+		printf("%d\n", x[i]);
+		i++;
+	}
+	i = 0;
+	printf("\n-----------Vetor original char-----------\n\n%p\n", fts);
+	while (i < 5)
+	{
+		printf("%d\n", fts[i]);
+		i++;
+	}
+	i = 0;
+	ft_bzero(fts, 2);
+	printf("\n-----------Vetor após função ft char------------\n\n%p\n", fts);
+	while (i < 5)
+	{
+		printf("%d\n", fts[i]);
+		i++;
+	}
+	i = 0;
+	printf("\n-----------Vetor original char-----------\n\n%p\n", s);
+	while (i < 5)
+	{
+		printf("%d\n", s[i]);
+		i++;
+	}
+	i = 0;
+	bzero(s, 2);
+	printf("\n-----------Vetor após função original char------------\n\n%p\n", s);
+	while (i < 5)
+	{
+		printf("%d\n", s[i]);
+		i++;
+	}
 }
--- ft_memset.c
+++ ft_memset.c
@@ -10,6 +10,7 @@ void	*ft_memset(void *s, int c, size_t n)
 	while(i < n)
 	{
 		s1[i] = c;
+		// s1++;
 		i++;
 	}
 	return ((void *)s1);
@@ -19,34 +20,37 @@ void	*ft_memset(void *s, int c, size_t n)
 #include <string.h>
 int	main()
 {
-	int s[5] = {49, 49, 49, 49, 49};
+	int ftx[5] = {49, 49, 49, 49, 49};
+	// char fts[5] = {48, 48, 48, 48, 48};
 	// char s[5] = {48, 48, 48, 48, 48};
+	int x[5] = {49, 49, 49, 49, 49};
 	int i = 0;
 
-	printf("\n-----------Vetor original-----------\n\n%p\n", s);
+	printf("\n-----------Vetor original-----------\n\n%p\n", ftx);
 	while (i < 5)
 	{
-		printf("%c\n", s[i]);
+		printf("%d\n", ftx[i]);
 		i++;
 	}
 	i = 0;
-	printf("\n-----------Vetor após função ft------------\n\n%p\n", ft_memset(s, 120, 2));
+	printf("\n-----------Vetor após função ft------------\n\n%p\n", ft_memset(ftx, 120, 8));
 	while (i < 5)
 	{
-		printf("%c\n", s[i]);
+		printf("%d\n", ftx[i]);
 		i++;
 	}
-	// printf("\n-----------Vetor original-----------\n\n%p\n", s);
-	// while (i < 5)
-	// {
-	// 	printf("%c\n", s[i]);
-	// 	i++;
-	// }
-	// i = 0;
-	// printf("\n-----------Vetor após função original------------\n\n%p\n", memset(s, 120, 2));
-	// while (i < 5)
-	// {
-	// 	printf("%c\n", s[i]);
-	// 	i++;
-	// }
+	i = 0;
+	printf("\n-----------Vetor original-----------\n\n%p\n", x);
+	while (i < 5)
+	{
+		printf("%d\n", x[i]);
+		i++;
+	}
+	i = 0;
+	printf("\n-----------Vetor após função original------------\n\n%p\n", memset(x, 120, 8));
+	while (i < 5)
+	{
+		printf("%d\n", x[i]);
+		i++;
+	}
 }
