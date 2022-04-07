623bc22174d5bc984358d9a58b834c3298eae36f
asoler <asoler@student.42sp.org.br>
2022-04-07 21:06:21 +0200

    strrchar and correcting return to null when c is not fid in s

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,35 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/06 22:03:00 by asoler            #+#    #+#             */
-/*   Updated: 2022/04/07 21:05:19 by asoler           ###   ########.fr       */
+/*   Created: 2022/04/07 20:40:36 by asoler            #+#    #+#             */
+/*   Updated: 2022/04/07 21:02:15 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strchr(const char *s, int c)
+char *ft_strrchr(const char *s, int c)
 {
-	while (*s != '\0' && *s != c)
+	int	i;
+	int	result;
+
+	i = 0;
+	result = 0;
+	while (*s != '\0')
 	{
+		if (*s == c)
+		{
+			result = i;
+		}
 		s++;
+		i++;
 	}
-	if (*s == '\0')
+	if (result == 0)
 		return ((char *)0);
 	else
-		return ((char *)s);
+	{
+		i -= result;
+		return ((char *)s - i);
+	}
 }
 
 #include <stdio.h>
 #include <string.h>
 int	main()
 {
-	char	s[] = "lest deliver  position";
+	char	s[] = "lets deliver  last position";
 
-	printf("\n\n   ---ft---\n%p\n", ft_strchr(s, 'x'));
-	printf("\n\n   ---ft---\n%s\n", ft_strchr(s, 'x'));
-	printf("\n\n   ---original---\n%p\n", strchr(s, 'x'));
-	printf("\n\n   ---original---\n%s\n", strchr(s, 'x'));
+	printf("\n\n   ---ft---\n%p\n", ft_strrchr(s, 'x'));
+	printf("\n\n   ---ft---\n%s\n", ft_strrchr(s, 'x'));
+	printf("\n\n   ---original---\n%p\n", strrchr(s, 'x'));
+	printf("\n\n   ---original---\n%s\n", strrchr(s, 'x'));
 }
