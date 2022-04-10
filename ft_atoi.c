ba25b84cfcdd0bd3878b63b890e9afa8ff43f668
asoler <asoler@student.42sp.org.br>
2022-04-10 22:23:48 +0200

    wip atoi, make an aux funtion to count an power 10

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,26 +6,42 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 18:52:29 by asoler            #+#    #+#             */
-/*   Updated: 2022/04/10 19:08:19 by asoler           ###   ########.fr       */
+/*   Updated: 2022/04/10 22:22:07 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *nptr)
 {
 	char	signal;
+	int		i;
 	int		result;
+	// int		len;
 
 	signal = 'x';
+	i = 0;
+	// len = ft_strlen(nptr);
+	if (!((nptr[0] == '-' || nptr[0] == '+') || (nptr[0] >= '0' && nptr[0] <= '9')))
+	// if first position of pointer is not signal or number
+		return (0);
 	if (nptr[0] == '-' || nptr[0] == '+')
+	{
 		signal = nptr[0];
-	else if (*nptr >= '0' && *nptr <= '9')
-		
-	else
+		i = 1;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+		i++;
+	if (signal == 'x')
+		result = ft(
+			
+		);
+	return (result);
 }
 
 #include <stdlib.h>
 #include <stdio.h>
 int	main()
 {
-	printf("    =====original=====\n%d\n", atoi("2147483650"));
+	char	n[] = "5";
+	printf("    =====ft=====\n%d\n", ft_atoi(n));
+	printf("    =====original=====\n%d\n", atoi(n));
 }
