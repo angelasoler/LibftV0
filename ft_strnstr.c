6cf0b3dfe10d178f4b458f8617d9ac0abf968414
asoler <asoler@student.42sp.org.br>
2022-04-10 03:44:26 +0200

    wip strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,21 +6,24 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 21:05:30 by asoler            #+#    #+#             */
-/*   Updated: 2022/04/09 21:23:30 by asoler           ###   ########.fr       */
+/*   Updated: 2022/04/09 21:29:39 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// char	*ft_strnstr(const char *big, const char *little, size_t len)
-// {
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	char *ptr;
+
 	
-// }
+	return (ptr);
+}
 
 #include <stdio.h>
 #include <string.h>
 int	main()
 {
 	char	big[] = "where is null";
-	char	little[] = "here is nullx";
+	char	little[] = "where is null here";
 	// int		len = 15;
 
 	printf("    ======original======\n%s\n", strstr(big, little));
