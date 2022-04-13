ae168127e3cd7852254aca31dee8a0eced124b13
asoler <asoler@student.42sp.org.br>
2022-04-13 03:53:49 +0200

    memcmp => chars of str most be compare as unsigned char

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 20:08:30 by asoler            #+#    #+#             */
-/*   Updated: 2022/04/12 21:41:23 by asoler           ###   ########.fr       */
+/*   Updated: 2022/04/13 03:50:43 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	char	*s_s1;
-	char	*s_s2;
+	unsigned char	*s_s1;
+	unsigned char	*s_s2;
 
-	s_s1 = (char *)s1;
-	s_s2 = (char *)s2;
+	s_s1 = (unsigned char *)s1;
+	s_s2 = (unsigned char *)s2;
 	while (n != 0)
 	{
 		if (*s_s1 != *s_s2)
