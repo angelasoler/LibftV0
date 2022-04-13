7739285559a5b3d4db7d2349756efd0fcae11dfe
asoler <asoler@student.42sp.org.br>
2022-04-13 03:25:16 +0200

    strchr => convertion of c to unisgned char and return pointer to terminator if c = 0

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,13 +6,22 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 22:03:00 by asoler            #+#    #+#             */
-/*   Updated: 2022/04/09 20:02:32 by asoler           ###   ########.fr       */
+/*   Updated: 2022/04/13 03:22:20 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != '\0' && *s != c)
+	unsigned char	a;
+
+	if (c == 0)
+	{
+		return ((char *)&s[ft_strlen(s)]);
+	}
+	a = (unsigned char)c;
+	while (*s != '\0' && *s != a)
 	{
 		s++;
 	}
