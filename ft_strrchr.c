7f8381389944b298bd14930e9a04ae77972cde1c
asoler <asoler@student.42sp.org.br>
2022-04-13 03:37:18 +0200

    strRchr if c = 0 return ptr to terminatr, resultwas 0 avoiding c in index 0

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,27 +6,31 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 20:40:36 by asoler            #+#    #+#             */
-/*   Updated: 2022/04/09 20:05:32 by asoler           ###   ########.fr       */
+/*   Updated: 2022/04/13 03:34:46 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 	int	result;
+	unsigned char	a;
 
+	if (c == 0)
+		return ((char *)&s[ft_strlen(s)]);
+	a = (unsigned char)c;
 	i = 0;
-	result = 0;
-	while (*s != '\0')
+	result = -1;
+	while (*s)
 	{
-		if (*s == c)
-		{
+		if (*s == a)
 			result = i;
-		}
 		s++;
 		i++;
 	}
-	if (result == 0)
+	if (result == -1)
 		return ((char *)0);
 	else
 	{
