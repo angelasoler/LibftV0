1a48f3085101bd9f0f868d6e252f7e86ea8be517
asoler <asoler@student.42sp.org.br>
2022-04-13 16:50:12 +0200

    wip split => passing norma

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 17:44:10 by asoler            #+#    #+#             */
-/*   Updated: 2022/04/13 16:25:52 by asoler           ###   ########.fr       */
+/*   Updated: 2022/04/13 16:48:20 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,39 @@ static int	count_words(char const *s, char c)
 	return (wc);
 }
 
+static char	**result_array(char	**dest, char *s1, char c)
+{
+	char	*beg;
+	char	*end;
+	int		i;
+
+	end = s1;
+	i = 0;
+	while (*s1)
+	{
+		beg = end;
+		while (*s1 != c && *s1 != '\0')
+			s1++;
+		*s1 = '\0';
+		s1++;
+		end = s1;
+		dest[i] = malloc(sizeof(char *) * (ft_strlen(beg) + 1));
+		dest[i] = ft_strdup(beg);
+		while (*end == c && *s1 != '\0')
+		{
+			end++;
+			s1++;
+		}
+		i++;
+	}
+	dest[i] = (char *)0;
+	return (dest);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
 	char	*s1;
-	char	*beg;
-	char	*end;
-	int		i;
 	int		size;
 
 	if (s == 0 && c == 0)
@@ -51,27 +77,8 @@ char	**ft_split(char const *s, char c)
 	s1 = ft_strdup(s);
 	size = count_words(s, c);
 	result = malloc(sizeof(char *) * size);
-	i = 0;
 	while (*s1 == c)
 		s1++;
-	end = s1;
-	while (*s1)
-	{
-		beg = end;
-		while (*s1 != c && *s1 != '\0')
-			s1++;
-		*s1 = '\0';
-		s1++;
-		end = s1;
-		result[i] = malloc(sizeof(char *) * (ft_strlen(beg) + 1));
-		result[i] = ft_strdup(beg);
-		while (*end == c && *s1 != '\0')
-		{
-			end++;
-			s1++;
-		}
-		i++;
-	}
-	result[i] = (char *)0;
+	result = result_array(result, s1, c);
 	return (result);
 }
