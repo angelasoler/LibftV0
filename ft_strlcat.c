c729e890bc2c4115db4aee245030a3f8ff7e5911
asoler <asoler@student.42sp.org.br>
2022-04-13 22:13:00 +0200

    wip strlcat => return case when count is 0 and return is des len + src len

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 21:17:45 by asoler            #+#    #+#             */
-/*   Updated: 2022/04/12 22:21:58 by asoler           ###   ########.fr       */
+/*   Updated: 2022/04/13 21:49:14 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,27 @@ size_t	ft_strlcat(char *dest, const char *src, size_t count)
 {
 	unsigned int	i;
 	int				len;
+	int				total_len;
 
 	i = 0;
-	len = ft_strlen(dest);
+	if (count <= 0)
+		len = 0;
+	else
+		len = ft_strlen(dest);
+	total_len = ft_strlen(src) + len;
+	if (count <= 0)
+		return (total_len);
 	while (i < count)
 	{
 		if (src[i] == '\0')
 		{
-			dest[len] = src[i];
-			return (0);
+			dest[len] = '\0';
+			return (total_len);
 		}
 		dest[len] = src[i];
 		len++;
 		i++;
 	}
 	dest[len - 1] = '\0';
-	return (0);
+	return (total_len);
 }
